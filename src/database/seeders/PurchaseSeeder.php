b180a2e8dbebf00083b0164097ce62667cb83c8f
Keiichi Kikuchi <totetu.kikuchi@gmail.com>
2025-05-29 12:24:54 +0900

    -70

--- src/database/seeders/PurchaseSeeder.php
+++ src/database/seeders/PurchaseSeeder.php
@@ -4,17 +4,15 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use AppModels\Purchase;
 
-class CustomerSeeder extends Seeder
+class PurchaseSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run()
+    public function run(): void
     {
-        $this->call([
-            UserSeeder::class,
-            ItemSeeder::class
-        ]);
+        //
     }
 }
