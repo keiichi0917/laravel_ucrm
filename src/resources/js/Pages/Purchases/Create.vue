8e7cade1fcff9cfbf82b07f06b1cdeee699c97c7
Keiichi Kikuchi <totetu.kikuchi@gmail.com>
2025-05-29 14:46:23 +0900

    -72

---
+++ src/resources/js/Pages/Purchases/Create.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { getToday } from '@/common'; // 別ファイルをインポート
+import { onMounted, reactive } from 'vue';
+onMounted(() => {
+    // ページ読み込み後 即座に実行
+    form.date = getToday();
+});
+const form = reactive({ date: null });
+</script>
+<template>
+    　日付<br />
+    <input type="date" name="date" v-model="form.date" />
+</template>
--- src/resources/js/common.js
+++ src/resources/js/common.js
@@ -3,4 +3,12 @@ const nl2br = (str) => {
   return str.replace(/\n/g, '<br>')
 }
 
-export  { nl2br }
+const getToday = () => {
+ const today = new Date();
+ const yyyy = today.getFullYear();
+ const mm = ("0"+(today.getMonth()+1)).slice(-2);
+ const dd = ("0"+today.getDate()).slice(-2);
+ return yyyy+'-'+mm+'-'+dd;
+}
+
+export  { nl2br , getToday }
